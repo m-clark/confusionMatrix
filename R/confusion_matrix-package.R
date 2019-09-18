984106f058a226d96516354130472c363585e92b
Michael Clark <micl@umich.edu>
2019-09-18 16:24:00 -0400

    update package description

--- R/confusion_matrix-package.R
+++ R/confusion_matrix-package.R
@@ -1,12 +1,41 @@
 #' @name confusionMatrix
 
 #' @description  The goal of this package is primarily to provide an easy
-#'   way to provide confusion table metrics in a tidy fashion.  The inspiration
-#'   and a good chunk of the code borrows heavily from Max Kuhn's \code{caret}
-#'   package and associated function \code{confusionMatrix}.  Here, practically
-#'   all dependencies have been removed outside of dplyr, and results return
-#'   tibbles making for easier document presentation, as well as the ability to
-#'   peel off the statistics desired.
+#'   way to obtain common confusion table metrics in a tidy fashion.  The
+#'   inspiration and a good chunk of the code borrows heavily from Max Kuhn's
+#'   \code{caret} package and associated function \code{confusionMatrix}, and
+#'   his current work being done via the \code{yardstick} package.  Here,
+#'   practically all dependencies have been removed except for dplyr, and
+#'   results are tibbles making for easier document presentation, as well as the
+#'   ability to peel off the statistics desired.
+#'
+#'   All that is required is a vector of predicted classes and a vector of
+#'   observed classes, as that is typically what we're dealing with in such
+#'   scenarios.  These can be logical, integer/numeric, character, or factor,
+#'   but the predictions should match the observations in an obvious way.
+#'
+#'   Statistics provided include:
+#'
+#'   Accuracy and Agreement
+#'     - Accuracy, bounds, and related
+#'     - Cohen's Kappa
+#'     - Corrected Rand
+#'
+#'   Other Statistics:
+#'     - Sensitivity
+#'     - Specificity
+#'     - Prevalence
+#'     - Positive Predictive Value
+#'     - Negative Predictive Value
+#'     - Detection prevalence
+#'     - Balanced Accuracy
+#'     - F1
+#'
+#'   Measures of Association:
+#'     - Phi
+#'     - Yule's
+#'     - Peirce's science of the method (Youden's J)
+#'
 
 #' @keywords internal
 "_PACKAGE"
